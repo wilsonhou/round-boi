4a0f1db7cca0c28f7cd63163d305077f7478a578
Wilson Hou <wilshou@gmail.com>
2022-06-19 01:09:18 +1000

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
+// Go after the things that will probably be the hardest.
 const Home: NextPage = () => {
   return (
     <div>
@@ -10,9 +11,16 @@ const Home: NextPage = () => {
         <meta name="description" content="app to find roundest boi" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-5xl'>hi world !!</h1>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <h1 className="text-2xl text-center">Which Pokemon is the roundest?</h1>
+        <div className="border rounded p-8 flex justify-between">
+          <div className="h-36 w-36 bg-yellow-200">First</div>
+          <div className="m-4">VS</div>
+          <div className="h-36 w-36 bg-red-200">Second</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
